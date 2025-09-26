1286e1680b7a4100f379c78f57b087c5f516d4e4
aishwariya-ai <sri.aishwariya.murugan@gmail.com>
2025-09-26 16:30:54 +0000

    Code challenge submission for question: 136 Single Number

---
+++ 136 Single Number/136single-number.py
@@ -0,0 +1,23 @@
+'''class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        for i in range(len(nums)):
+            flag=True
+            for j in range(len(nums)):
+                if i!=j and nums[i]==nums[j]:
+                    flag=False
+                    break
+                    
+            if(flag):
+                return nums[i]'''
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        for num in nums:
+            result ^= num
+        return result
+
+
+
+        
