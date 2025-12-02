f72503c9ec1389a7e189cd86125d23e0ea7f7c99
aishwariya-ai <sri.aishwariya.murugan@gmail.com>
2025-12-02 14:58:42 +0000

    Code challenge submission for question: 238 Product of Array Except Self

---
+++ 238 Product of Array Except Self/238product-of-array-except-self.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        res=[1]*len(nums)
+        prev=1
+        for i in range(len(nums)):
+            res[i]=prev
+            prev*=nums[i]
+        post=1
+        for i in range(len(nums)-1,-1,-1):
+            res[i]*=post
+            post*=nums[i]
+        return res
+
+        
