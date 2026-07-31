75898c5524005d271a42ad67fa8c85fe2dcc7a04
aishwariya-ai <sri.aishwariya.murugan@gmail.com>
2026-07-31 11:27:15 +0000

    Code challenge submission for question: 125 Valid Palindrome

---
+++ 125 Valid Palindrome/125valid-palindrome.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        clean=""
+        for char in s:
+            if(char.isalnum()):
+                clean+=char.lower()
+        return clean==clean[::-1]
+        
