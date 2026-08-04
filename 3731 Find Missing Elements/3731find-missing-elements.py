eb14f23e32dd3e9312f7b6ebd66af9f4737da639
aishwariya-ai <sri.aishwariya.murugan@gmail.com>
2026-08-04 07:27:33 +0000

    Code challenge submission for question: 3731 Find Missing Elements

---
+++ 3731 Find Missing Elements/3731find-missing-elements.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findMissingElements(self, nums):
+        mini=min(nums)
+        maxi=max(nums)
+        arr=[]
+        for i in range(mini,maxi+1):
+            if i not in nums:
+                arr.append(i)
+        return sorted(arr)
+                
+        
