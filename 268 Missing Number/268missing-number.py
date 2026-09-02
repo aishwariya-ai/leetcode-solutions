898dd6e046771f836c86a3b52d6cd2b3b05395f8
aishwariya-ai <sri.aishwariya.murugan@gmail.com>
2026-09-02 04:55:45 +0000

    Code challenge submission for question: 268 Missing Number

---
+++ 268 Missing Number/268missing-number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        for i in range(len(nums)+1):
+            if(i not in nums):
+                return i
+                
+
+            
+        
