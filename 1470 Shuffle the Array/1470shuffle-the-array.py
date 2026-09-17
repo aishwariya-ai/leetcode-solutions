ecd268b7bf1d87f8ebef32b581a4714dcd422a96
aishwariya-ai <sri.aishwariya.murugan@gmail.com>
2026-09-17 04:58:34 +0000

    Code challenge submission for question: 1470 Shuffle the Array

---
+++ 1470 Shuffle the Array/1470shuffle-the-array.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def shuffle(self, nums, n):
+        arr=[]
+        for i in range(n):
+            arr.append(nums[i])
+            arr.append(nums[i+n])
+        return arr
